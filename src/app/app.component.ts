9c434c88da4a21386cb5a8b81ec0298e20589e42
Aleksandra Volynets <saxara_a@mail.ru>
2023-12-26 01:41:47 +0300

    First task finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'geo-app';
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import {fromLonLat} from 'ol/proj.js';
 
 @Component({
   selector: 'app-map',
@@ -7,6 +12,24 @@ import { Component } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
-export class MapComponent {
 
+export class MapComponent implements AfterViewInit {
+  map: Map | undefined;
+
+  ngAfterViewInit(): void {
+      if (typeof document != 'undefined') {
+        this.map = new Map({
+          view: new View({
+            center: fromLonLat([7.0839985, 50.7378408]), // Coordinates for Eifel Str. 20, Bonn, Germany
+            zoom: 19 // Default zoom level
+          }),
+          layers: [
+            new TileLayer({
+              source: new OSM(),
+            }),
+          ],
+          target: 'map'
+        });
+      }
+  }
 }
