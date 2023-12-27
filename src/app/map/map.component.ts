aa7ed49160e80247ac5ebba6e6631b8f200a8b3e
theantialex <saxara_a@mail.ru>
2023-12-27 13:52:01 +0300

    Added more code comments

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,16 +3,16 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import {fromLonLat, toLonLat} from 'ol/proj.js';
-import { Feature, Overlay } from 'ol';
-import { TitleCasePipe } from '@angular/common'
+import { fromLonLat, toLonLat } from 'ol/proj.js';
+import Overlay from 'ol';
+import TitleCasePipe from '@angular/common'
 import GeoJSON from 'ol/format/GeoJSON.js';
 import { TileWMS, Vector as VectorSource } from 'ol/source';
 import { Vector as VectorLayer } from 'ol/layer';
-import { defaults } from 'ol/interaction/defaults';
+import defaults from 'ol/interaction/defaults';
 import Style from 'ol/style/Style';
 import Stroke from 'ol/style/Stroke';
-import {getLength} from 'ol/sphere.js'
+import getLength from 'ol/sphere.js'
 
 
 @Component({
@@ -26,78 +26,73 @@ import {getLength} from 'ol/sphere.js'
 export class MapComponent implements AfterViewInit {
   map: Map | undefined;
   popup: Overlay | undefined;
-  title: string = 'Not found';
-  content: string = '';
+  title: string = 'Not found'; // popup title
+  content: string = ''; // popup content
   vectorSource: VectorSource;
   vectorLayer: VectorLayer<VectorSource>;
   wmsLayer: TileLayer<TileWMS>;
-  wmsSetting: boolean = false;
+  wmsSetting: boolean = false; // initial WMS layer status (false = not visible)
 
   constructor() {
     this.vectorSource = new VectorSource();
     this.vectorLayer = new VectorLayer({
-      source: this.vectorSource,
-      style: {
-        'stroke-color': 'red',
-        'stroke-width': 2,
-      }
+      source: this.vectorSource
     });
 
     this.wmsLayer = new TileLayer({
-              source: new TileWMS({
-                url: "https://gitc.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi",
-                params: {
-                  'layers':'MODIS_Terra_L3_Land_Surface_Temp_Monthly_Day'
-                },
-                serverType: 'geoserver',
-                transition: 0
-              }),
-              visible: true
+      source: new TileWMS({
+        url: "https://gitc.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi", // WMS map from NASA OGC Web Map Service API
+        params: {
+          'layers': 'MODIS_Terra_L3_Land_Surface_Temp_Monthly_Day' // Land Surface Tempreture Layer
+        },
+        serverType: 'geoserver',
+        transition: 0
+      }),
+      visible: true
     })
   }
 
   ngAfterViewInit(): void {
-      if (typeof document != 'undefined') {
-
-        this.map = new Map({
-          interactions: defaults({ doubleClickZoom: false }),
-          view: new View({
-            center: fromLonLat([7.0839985, 50.7378408]), // Coordinates for Eifel Str. 20, Bonn, Germany
-            zoom: 19 // Default zoom level
+    if (typeof document != 'undefined') {
+      this.map = new Map({
+        interactions: defaults({ doubleClickZoom: false }), // removing default double click event
+        view: new View({
+          center: fromLonLat([7.0839985, 50.7378408]), // coordinates for Eifel Str. 20, Bonn, Germany
+          zoom: 19 // default zoom level
+        }),
+        layers: [
+          new TileLayer({
+            source: new OSM(),
           }),
-          layers: [
-            new TileLayer({
-              source: new OSM(),
-            }),
-            this.vectorLayer
-          ],
-          target: 'map'
-        });
+          this.vectorLayer
+        ],
+        target: 'map'
+      });
 
-        let elem = document.getElementById('popup');
-        if (elem) {
-          this.popup = new Overlay({
-            element: elem
-          });
-          this.map.addOverlay(this.popup);
-        }
-    
-        this.map.on('click', (event) => {
-          if (this.map) {
-            var point = this.map.getCoordinateFromPixel(event.pixel);
-            this.displayPopup(point);
-            this.removeVectorLayer();
-          }
+      let elem = document.getElementById('popup');
+      if (elem) {
+        this.popup = new Overlay({
+          element: elem
         });
-
-        this.map.on('dblclick', (event) => {
-          if (this.map) {
-            var point = this.map.getCoordinateFromPixel(event.pixel);
-            this.refreshData(point);
-          }
-        });
-
+        this.map.addOverlay(this.popup);
       }
+
+      this.map.on('click', (event) => {
+        if (this.map) {
+          var point = this.map.getCoordinateFromPixel(event.pixel); // getting click event coordinates
+          this.displayPopup(point);
+          this.removeVectorLayer();
+        }
+      });
+
+      this.map.on('dblclick', (event) => {
+        if (this.map) {
+          var point = this.map.getCoordinateFromPixel(event.pixel); // getting click event coordinates
+          this.refreshData(point);
+        }
+      });
+
+    }
   }
 
   removeVectorLayer() {
@@ -105,62 +100,63 @@ export class MapComponent implements AfterViewInit {
   }
 
   async getInfo(coordinate: number[]) {
-    const responce = await fetch('https://api.geoapify.com/v1/geocode/reverse?lon=' + coordinate[0] + '&lat=' + coordinate[1] + '&format=json&apiKey=6a72d6b8fafa4062acb947e892b30b0d')
+    const responce = await fetch('https://api.geoapify.com/v1/geocode/reverse?lon=' + coordinate[0] + '&lat=' + coordinate[1] + '&format=json&apiKey=6a72d6b8fafa4062acb947e892b30b0d') // api key should be removed from public repository - it is left here for the sake of simplicity
     return responce.json();
   }
 
   displayPopup(coordinate: number[]): void {
-      this.getInfo(toLonLat(coordinate)).then(
-        (result) => { 
-          this.title = result['results'][0]['result_type'];
-          this.content = "Address: " + result['results'][0]['formatted'];
+    this.getInfo(toLonLat(coordinate)).then( // getting actual map coordinates from click event coordinates
+      (result) => {
+        this.title = result['results'][0]['result_type']; // parsing API responce
+        this.content = "Address: " + result['results'][0]['formatted']; // parsing API responce
 
-          if (this.popup != undefined) {
-            this.popup.setPosition(coordinate);
-          }
+        if (this.popup != undefined) {
+          this.popup.setPosition(coordinate);
+        }
       }).catch((error) => {
         console.error('Error fetching location information:', error);
       });
   }
-    
+
   closePopup(): void {
     if (this.popup != undefined) {
       this.popup.setPosition(undefined);
     }
   }
 
+  // getting GeoJSON Feature style dynamically based on its geometry length
   getColor(feature: any) {
-    let colors = ['green', 'black']
-    let ind = 0;
+    let colors = ['green', 'black'] // random example colors
+    let ind = 0; // default color choice
     let geom = feature.getGeometry()
     if (geom) {
-      ind = Math.round(getLength(geom)) % 2
+      ind = Math.round(getLength(geom)) % 2 // choosing color based on if length is even or odd
     }
     return colors[ind];
   }
 
 
   async refreshData(coordinate: number[]) {
-    let lastLocation = toLonLat(coordinate);
-    let newVectorLayer = new VectorLayer({
+    let lastLocation = toLonLat(coordinate); // getting actual map coordinates from click event coordinates
+    let newVectorLayer = new VectorLayer({ // updating GeoJSON data from API
       source: new VectorSource({
-      url: 'https://api.geoapify.com/v1/isoline?lon='+ lastLocation[0] + '&lat=' + lastLocation[1] + '&type=time&mode=walk&range=300&range=600&range=1800'
-            + '&apiKey=6a72d6b8fafa4062acb947e892b30b0d',
-            format: new GeoJSON(),
+        url: 'https://api.geoapify.com/v1/isoline?lon=' + lastLocation[0] + '&lat=' + lastLocation[1] + '&type=time&mode=walk&range=300&range=600&range=1800'
+          + '&apiKey=6a72d6b8fafa4062acb947e892b30b0d',  // api key should be removed from public repository - it is left here for the sake of simplicity
+        format: new GeoJSON(),
       }),
-      style:  (feature: any) => {
+      style: (feature: any) => {
         return new Style({
           stroke: new Stroke({
-            color: this.getColor(feature),
+            color: this.getColor(feature), // getting style dynamically
             width: 3,
           }),
         });
       }
     })
-        
+
     this.map?.addLayer(newVectorLayer);
     this.vectorLayer = newVectorLayer;
-    this.map?.getView().setZoom(15);
+    this.map?.getView().setZoom(15); // zooming out in order to see accessable areas properly
   }
 
   toggleWMSLayerVisibility() {
